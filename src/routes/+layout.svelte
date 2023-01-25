886f6b4b175278775ffc62266ee67905f8e8dc7f
JhessReyes <jhessaureyes@gmail.com>
2023-01-25 14:14:51 -0600

    tailwind

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,14 @@
 
 	<main>
 		<slot />
+
+		<h1 class="text-3xl font-bold underline">Hello world!</h1>
+
+		<style lang="postcss">
+			:global(html) {
+				background-color: theme(colors.gray.100);
+			}
+		</style>
 	</main>
 
 	<footer>
